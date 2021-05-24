deea5b02b7d880e677e942bb88c8c442b0c99978
SRDewan <shaurya.dewan@students.iiit.ac.in>
2021-05-24 15:44:23 +0530

    Added experiment name and template. Done with graphics and half of animation.

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,526 @@
-//Your JavaScript goes in here
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function(){
+
+	const restartButton = document.getElementById('restart');
+	const instrMsg = document.getElementById('procedure-message');
+
+	restartButton.addEventListener('click', function() {restart();});
+
+	function randomNumber(min, max) {
+		return (Math.random() * (max - min + 1) + min).toFixed(2);
+	};
+
+	function logic(tableData)
+	{
+		const soilData = { 'Silt': randomNumber(22.5, 27.5), 'Sand': randomNumber(12, 16), 'Clay': randomNumber(30, 50) };
+		tableData.forEach(function(row, index) {
+			const ans = soilData[row['Soil Type']];
+			row['Water Content(%)'] = ans;
+			row['Dry Soil Mass(g)'] = ((1 - (ans / 100)) * wetSoilMass).toFixed(2);
+		});
+	};
+
+	function limCheck(obj, translate, lim, step)
+	{
+		if(obj.pos[0] === lim[0])
+		{
+			translate[0] = 0;
+		}
+
+		if(obj.pos[1] === lim[1])
+		{
+			translate[1] = 0;
+		}
+
+		if(translate[0] === 0 && translate[1] === 0)
+		{
+			if(step === 2)
+			{
+				document.getElementById("output1").innerHTML = "Mass of cutter = " + String(10) + "g";
+			}
+
+			else if(step === 4)
+			{
+				document.getElementById("output2").innerHTML = "Mass of wet soil = " + String(wetSoilMass) + "g";
+			}
+
+			else if(step === 8)
+			{
+				logic(tableData);
+				for(let i = 0; i < tables.length; i += 1)
+				{
+					generateTableHead(tables[i], Object.keys(tableData[0]));
+					generateTable(tables[i], tableData);
+				}
+
+				document.getElementById("apparatus").style.display = 'none';
+				document.getElementById("observations").style.width = '40%';
+				if(small)
+				{
+					document.getElementById("observations").style.width = '85%';
+					document.getElementById("observations").style.marginLeft = '7.5%';
+				}
+			}
+			return step + 1;
+		}
+
+		return step;
+	};
+
+	function updatePos(obj, translate, lim, step)
+	{
+		obj.pos[0] += translate[0];
+		obj.pos[1] += translate[1];
+	};
+
+	class soil {
+		constructor(height, width, radius, x, y) {
+			this.height = height;
+			this.width = width;
+			this.radius = radius;
+			this.pos = [x, y];
+		};
+
+		draw(ctx) {
+			if (this.width < 2 * this.radius) 
+			{
+				this.radius = this.width / 2;
+			}
+
+			if (this.height < 2 * this.radius) 
+			{
+				this.radius = this.height / 2;
+			}
+
+			ctx.beginPath();
+			ctx.fillStyle = "#654321";
+			ctx.lineWidth = lineWidth;
+			ctx.beginPath();
+	
+			ctx.moveTo(this.pos[0] + this.radius, this.pos[1]);
+			ctx.arcTo(this.pos[0] + this.width, this.pos[1], this.pos[0] + this.width, this.pos[1] + this.height, this.radius);
+			ctx.arcTo(this.pos[0] + this.width, this.pos[1] + this.height, this.pos[0], this.pos[1] + this.height, this.radius);
+			ctx.arcTo(this.pos[0], this.pos[1] + this.height, this.pos[0], this.pos[1], this.radius);
+			ctx.arcTo(this.pos[0], this.pos[1], this.pos[0] + this.width, this.pos[1], this.radius);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		};
+
+		heating(unit) {
+			this.height -= unit;
+		};
+	};
+
+	class unevenSoil{
+		constructor(height, width, x, y) {
+			this.height = height;
+			this.width = width;
+			this.pos = [x, y];
+			this.img = new Image();
+			this.img.src = './images/uneven soil.png';
+			this.img.onload = () => {ctx.drawImage(this.img, this.pos[0], this.pos[1], this.width, this.height);}; 
+		};
+
+		draw(ctx) {
+			ctx.drawImage(objs['soil'].img, objs['soil'].pos[0], objs['soil'].pos[1], objs['soil'].width, objs['soil'].height);
+		}
+	};
+
+	class cutter{
+		constructor(height, width, x, y) {
+			this.height = height;
+			this.width = width;
+			this.pos = [x, y];
+			this.img = new Image();
+			this.img.src = './images/cutter.png';
+			this.img.onload = () => {ctx.drawImage(this.img, this.pos[0], this.pos[1], this.width, this.height);}; 
+		};
+
+		draw(ctx) {
+			ctx.drawImage(objs['cutter'].img, objs['cutter'].pos[0], objs['cutter'].pos[1], objs['cutter'].width, objs['cutter'].height);
+		}
+	};
+
+	class dolly{
+		constructor(height, width, x, y) {
+			this.height = height;
+			this.width = width;
+			this.pos = [x, y];
+			this.img = new Image();
+			this.img.src = './images/dolly.png';
+			this.img.onload = () => {ctx.drawImage(this.img, this.pos[0], this.pos[1], this.width, this.height);}; 
+		};
+
+		draw(ctx) {
+			ctx.drawImage(objs['dolly'].img, objs['dolly'].pos[0], objs['dolly'].pos[1], objs['dolly'].width, objs['dolly'].height);
+		}
+	};
+
+	class rammer{
+		constructor(height, width, x, y) {
+			this.height = height;
+			this.width = width;
+			this.pos = [x, y];
+			this.img = new Image();
+			this.img.src = './images/rammer.png';
+			this.img.onload = () => {ctx.drawImage(this.img, this.pos[0], this.pos[1], this.width, this.height);}; 
+		};
+
+		draw(ctx) {
+			ctx.drawImage(objs['rammer'].img, objs['rammer'].pos[0], objs['rammer'].pos[1], objs['rammer'].width, objs['rammer'].height);
+		}
+	};
+
+	class evenSoil{
+		constructor(height, width, x, y) {
+			this.height = height;
+			this.width = width;
+			this.pos = [x, y];
+			this.img = new Image();
+			this.img.src = './images/even soil.png';
+			this.img.onload = () => {ctx.drawImage(this.img, this.pos[0], this.pos[1], this.width, this.height);}; 
+		};
+
+		draw(ctx) {
+			ctx.drawImage(objs['soil'].img, objs['soil'].pos[0], objs['soil'].pos[1], objs['soil'].width, objs['soil'].height);
+		}
+	};
+
+	class weight{
+		constructor(height, width, x, y) {
+			this.height = height;
+			this.width = width;
+			this.pos = [x, y];
+			this.img = new Image();
+			this.img.src = './images/weighing machine.png';
+			this.img.onload = () => {ctx.drawImage(this.img, this.pos[0], this.pos[1], this.width, this.height);}; 
+		};
+
+		draw(ctx) {
+			ctx.drawImage(objs['weight'].img, objs['weight'].pos[0], objs['weight'].pos[1], objs['weight'].width, objs['weight'].height);
+		}
+	};
+
+	function init()
+	{
+		small = false;
+		document.getElementById("output1").innerHTML = "Mass of cutter = ____ g";
+		document.getElementById("output2").innerHTML = "Mass of wet soil = ____ g";
+
+		objs = {
+			"weight": new weight(270, 240, 90, 160),
+			"rammer": new rammer(70, 50, 690, 40),
+			"dolly": new dolly(60, 140, 140, 30),
+			"cutter": new cutter(150, 120, 630, 210),
+			"soil": new unevenSoil(150, 300, 450, 210),
+		};
+		keys = [];
+
+		enabled = [["weight"], ["weight", "cutter"], ["weight", "cutter"], ["weight", "cutter", "soil"], ["weight", "cutter", "soil"], ["cutter", "soil", "dolly"], ["cutter", "soil", "dolly", "rammer"], ["cutter", "soil", "dolly", "rammer"], ["cutter", "soil", "oven"], ["weight", "cutter", "soil"], []];
+		step = 0;
+		translate = [0, 0];
+		lim = [-1, -1];
+	};
+
+	function restart() 
+	{ 
+		window.clearTimeout(tmHandle); 
+
+		document.getElementById("inputForm").style.display = 'none';
+		document.getElementById("apparatus").style.display = 'block';
+		document.getElementById("observations").style.marginLeft = '0%';
+		document.getElementById("observations").style.width = '20%';
+		if(small)
+		{
+			document.getElementById("observations").style.width = '40%';
+		}
+
+		for(let i = 0; i < tables.length; i += 1)
+		{
+			tables[i].innerHTML = "";
+		}
+		init();
+
+		tmHandle = window.setTimeout(draw, 1000 / fps); 
+	};
+
+	function generateTableHead(table, data) {
+		let thead = table.createTHead();
+		let row = thead.insertRow();
+		data.forEach(function(key, ind) {
+			let th = document.createElement("th");
+			let text = document.createTextNode(key);
+			th.appendChild(text);
+			row.appendChild(th);
+		});
+	};
+
+	function generateTable(table, data) {
+		data.forEach(function(rowVals, ind) {
+			let row = table.insertRow();
+			Object.keys(rowVals).forEach(function(key, i) {
+				let cell = row.insertCell();
+				let text = document.createTextNode(rowVals[key]);
+				cell.appendChild(text);
+			});
+		});
+	};
+
+	function check(event, translate, step, flag=true)
+	{ 
+		if(translate[0] != 0 || translate[1] != 0)
+		{
+			return step;
+		}
+
+		const canvasPos = [(canvas.width / canvas.offsetWidth) * (event.pageX - canvas.offsetLeft), (canvas.height / canvas.offsetHeight) * (event.pageY - canvas.offsetTop)];
+		const errMargin = 10;
+
+		let hover = false, updateStep = false;
+		canvas.style.cursor = "default";
+		keys.forEach(function(val, ind, arr) {
+			if(canvasPos[0] >= objs[val].pos[0] - errMargin && canvasPos[0] <= objs[val].pos[0] + objs[val].width + errMargin && canvasPos[1] >= objs[val].pos[1] - errMargin && canvasPos[1] <= objs[val].pos[1] + objs[val].height + errMargin)
+			{
+				if(step === 2 && val === "cutter")
+				{
+					hover = true;
+					translate[0] = -5;
+					translate[1] = -5;
+					lim[0] = 150;
+					lim[1] = 80;
+				}
+
+				if(step === 4 && val === "soil")
+				{
+					hover = true;
+					if(flag)
+					{
+						objs['soil'] = new evenSoil(180, 300, 450, 180);
+						updateStep = true;
+					}
+				}
+
+				else if(step === 6 && val === "cutter")
+				{
+					hover = true;
+					translate[0] = 5;
+					translate[1] = 5;
+					lim[0] = 560;
+					lim[1] = 150;
+				}
+
+				else if(step === 7 && val === "oven" && canvasPos[0] >= objs[val].pos[0] - errMargin && canvasPos[0] <= objs[val].pos[0] + objs[val].width + errMargin && canvasPos[1] >= objs[val].pos[1] + objs[val].height * 0.8 - errMargin && canvasPos[1] <= objs[val].pos[1] + objs[val].height + errMargin)
+				{
+					hover = true;
+					translate[1] = 1;
+					lim[1] = 210;
+				}
+
+				else if(step === 8 && val === "cutter")
+				{
+					hover = true;
+					translate[0] = -5;
+					translate[1] = -5;
+					lim[0] = 135;
+					lim[1] = 110;
+				}
+			}
+		});
+
+		if(!flag && hover)
+		{
+			canvas.style.cursor = "pointer";
+			translate[0] = 0;
+			translate[1] = 0;
+			lim[0] = 0;
+			lim[1] = 0;
+		}
+
+		if(updateStep)
+		{
+			return step + 1;
+		}
+		
+		return step;
+	};
+
+	const sliders = ["soilMass"];
+	sliders.forEach(function(elem, ind) {
+		const slider = document.getElementById(elem);
+		const output = document.getElementById("demo_" + elem);
+		output.innerHTML = slider.value; // Display the default slider value
+
+		slider.oninput = function() {
+			output.innerHTML = this.value;
+			if(ind === 0)
+			{
+				wetSoilMass = this.value;
+			}
+		};
+	});
+
+	function curvedArea(ctx, e, gradX, gradY)
+	{
+		ctx.bezierCurveTo(e[0], e[1] += gradY, e[0] += gradX, e[1] += gradY, e[0] += gradX, e[1]);
+		ctx.bezierCurveTo(e[0] += gradX, e[1], e[0] += gradX, e[1] -= gradY, e[0], e[1] -= gradY);
+	};
+
+	const canvas = document.getElementById("main");
+	canvas.width = 840;
+	canvas.height = 400;
+	canvas.style = "border:3px solid";
+	const ctx = canvas.getContext("2d");
+
+	const fill = "#A9A9A9", border = "black", lineWidth = 1.5, fps = 150;
+	const msgs = [
+		"Add a 'Weighing Machine' from the apparatus menu.", 
+		"Add a 'Cutter' from the apparatus menu.",
+		"Click on the cutter to move it to the weighing machine and weigh it.",
+		"Set appropriate input values (Soil Mass) and add a 'Soil Sample' from the apparatus menu.",
+		"Click on the soil sample to even it out.",
+		"Add a 'Dolly' from the apparatus menu.", 
+		"Add a 'Rammer' from the apparatus menu.", 
+		"Click on the oven red portion to start the oven and heat the soil.",
+		"Click on the cutter with dry soil to weigh it.",
+		"Click the restart button to perform the experiment again.",
+	];
+
+	let step, translate, lim, objs, keys, enabled, small;
+	init();
+
+	const tableData = [
+		{ "Soil Type": "Silt", "Dry Soil Mass(g)": "", "Water Content(%)": "" },
+		{ "Soil Type": "Sand", "Dry Soil Mass(g)": "", "Water Content(%)": "" },
+		{ "Soil Type": "Clay", "Dry Soil Mass(g)": "", "Water Content(%)": "" },
+	];
+
+	const objNames = Object.keys(objs);
+	objNames.forEach(function(elem, ind) {
+		const obj = document.getElementById(elem);
+		obj.addEventListener('click', function(event) {
+			if(elem === "soil")
+			{
+				enabled[step].pop();
+				document.getElementById("inputForm").style.display = 'block';
+				return;
+			}
+
+			keys.push(elem);
+			step += 1;
+		});
+	});
+
+	// Input Parameters 
+	let wetSoilMass = 100, soilType = "Loam";
+	canvas.addEventListener('mousemove', function(event) {check(event, translate, step, false);});
+	canvas.addEventListener('click', function(event) {
+		step = check(event, translate, step);
+	});
+
+	const submitButton = document.getElementById("submit"), tables = document.getElementsByClassName("table");
+	submitButton.addEventListener('click', function(event) {
+		document.getElementById("inputForm").style.display = 'none';
+		enabled[step].push("soil");
+		keys.push("soil");
+		step += 1;
+	});
+
+	function responsiveTable(x) {
+		if(x.matches)	// If media query matches
+		{ 
+			small = true;
+			document.getElementById("observations").style.marginLeft = '0%';
+			document.getElementById("observations").style.width = '40%';
+			if(step === 9)
+			{
+				document.getElementById("observations").style.marginLeft = '7.5%';
+				document.getElementById("observations").style.width = '85%';
+			}
+		} 
+
+		else
+		{
+			small = false;
+			document.getElementById("observations").style.marginLeft = '0%';
+			document.getElementById("observations").style.width = '20%';
+			if(step === 9)
+			{
+				document.getElementById("observations").style.width = '40%';
+			}
+		}
+	};
+
+	let x = window.matchMedia("(max-width: 700px)");
+	responsiveTable(x); // Call listener function at run time
+	x.addListener(responsiveTable); // Attach listener function on state changes
+
+	function draw()
+	{
+		ctx.clearRect(0, 0, canvas.width, canvas.height); 
+		ctx.lineCap = "round";
+		ctx.lineJoin = "round";
+
+		let ctr = 0;
+		document.getElementById("main").style.pointerEvents = 'none';
+
+		objNames.forEach(function(name, ind) {
+			document.getElementById(name).style.pointerEvents = 'auto';
+			if(keys.includes(name) || !(enabled[step].includes(name)))
+			{
+				document.getElementById(name).style.pointerEvents = 'none';
+			}
+
+			if(keys.includes(name)) 
+			{
+				if(enabled[step].includes(name))
+				{
+					ctr += 1;
+				}
+				objs[name].draw(ctx);
+			}
+		});
+
+		if(ctr === enabled[step].length)
+		{
+			document.getElementById("main").style.pointerEvents = 'auto';
+		}
+
+		if(translate[0] != 0 || translate[1] != 0)
+		{
+			let temp = step;
+			const soilMoves = [6, 7, 8], cutterMoves = [2, 6, 8];
+
+			if(soilMoves.includes(step))
+			{
+				updatePos(objs['soil'], translate, lim, step);
+				if(step === 7)
+				{
+					objs['soil'].heating(translate[1]);
+				}
+
+				if(step === 4 || step === 7)
+				{
+					temp = limCheck(objs['soil'], translate, lim, step);
+				}
+			}
+
+			if(cutterMoves.includes(step))
+			{
+				updatePos(objs['cutter'], translate, lim, step);
+				temp = limCheck(objs['cutter'], translate, lim, step);
+			}
+
+			step = temp;
+		}
+
+		document.getElementById("procedure-message").innerHTML = msgs[step];
+		tmHandle = window.setTimeout(draw, 1000 / fps);
+	};
+
+	let tmHandle = window.setTimeout(draw, 1000 / fps);
+});
